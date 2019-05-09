a2d5e84c280c25856faccf166c1ffe6c24af522b
edgardo <edgarDuart2012@hotmail.com.ar>
2019-05-09 19:51:34 -0300

    primer commit

---
+++ EfCodeFirstCliente/Migrations/20190502233702_CreateSchoolDB.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EfCodeFirstCliente.Migrations
+{
+    public partial class CreateSchoolDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    GradeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GradeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Section = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.GradeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    GradeId = table.Column<int>(type: "int", nullable: true),
+                    Height = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Weight = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentID);
+                    table.ForeignKey(
+                        name: "FK_Students_Grades_GradeId",
+                        column: x => x.GradeId,
+                        principalTable: "Grades",
+                        principalColumn: "GradeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GradeId",
+                table: "Students",
+                column: "GradeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+        }
+    }
+}
---
+++ EfCodeFirstCliente/Program.cs
@@ -0,0 +1,34 @@
+﻿using EfCodeFirstCliente.Models;
+using System;
+using System.Linq;
+
+namespace EfCodeFirstCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "edgardo" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            using (var context = new SchoolContext())
+            {
+                var NombreEstudiante = context.Students
+                                                  .Where(s => s.StudentName == "Mariano")
+                                                  .Select(s => s.StudentID)
+                                                  .ToList();
+
+                foreach (var Nombre in NombreEstudiante)
+                {
+                    Console.WriteLine($"{Nombre}");
+                }
+            }
+            
+        }
+    }
+    
+}
